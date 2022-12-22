9f02d8c5c9d2a736c11de3f7e366bd725b89dda0
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-22 12:11:17 +0500

    number 1 >number 2 or  number 2 >number1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
+string s_num1 = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string s_num2 = Console.ReadLine();
+
+int num1 = Convert.ToInt32(s_num1);
+int num2 = Convert.ToInt32(s_num2);
+
+if (num1>num2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else 
+{
+    Console.WriteLine("Второе число больше первого");
+}
