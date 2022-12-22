934a8761e7e53eb34e807bdd387c716fdddaf998
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-22 12:50:37 +0500

    не работал git push

---
+++ dotnet_001/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+//Например:
+//● 4 -> 16
+//● -3 -> 9
+//● -7 -> 
+Console.WriteLine("Enter number: ");
+string enterNum = Console.ReadLine();
+int num = Convert.ToInt32(enterNum);
+
+int sqr = num * num;
+
+Console.WriteLine($"sqr = {sqr} ");    
---
+++ dotnet_002/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да
+//a = 2, b = 10 -> нет
+//a = 9, b = -3 -> да
+//a = -3 b = 9 -> нет
+ 
+Console.Write("Введите первое число: ");
+string s_num1 = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string s_num2 = Console.ReadLine();
+
+int num1 = Convert.ToInt32(s_num1);
+int num2 = Convert.ToInt32(s_num2);
+
+if (num1 == num2 *num2)
+{
+    Console.WriteLine("Первое число вляется квадратом второго");
+}
+else 
+{
+    Console.WriteLine("Первое число не вляется квадратом второго");
+}
